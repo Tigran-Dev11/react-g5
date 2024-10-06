6937a94330ed20337a1c1d34a6882c8e5330c2a7
SaqBaghdyan <baghdyan2000@gmail.com>
2024-10-06 15:46:32 +0400

    webRouting

--- src/webParts/home/index.jsx
+++ src/webParts/home/index.jsx
@@ -1,10 +1,10 @@
-import {Header} from "../../webPages/header/index"
-import {Footer} from "../../webPages/footer/index"
-import "../home/style.css"
-export const Home = ()=>{
-    <div>
-        <Header/>
-        <h1>HomePage</h1>
-        <Footer/>
-    </div>
-}
+import { Header } from "../../webPages/header/index";
+import { Footer } from "../../webPages/footer/index";
+import "../home/style.css";
+export const Home = () => {
+  <div>
+    <Header />
+    <h1>HomePage</h1>
+    <Footer />
+  </div>;
+};
