e4ef7f2d232ea7ce44d200b70798a85e9855af0c
SaqBaghdyan <baghdyan2000@gmail.com>
2024-10-17 16:57:46 +0400

    addproduct

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,3 @@
-import {Loading} from "../../components/loading" 
 
 
 export const Home = ()=>{
@@ -6,6 +5,5 @@ export const Home = ()=>{
   return  <div className="container">
        <h1>Այս կայքում կարող եք գտնել Խանութում առկա ամբողջ տեսականու մասին ողջ ինֆորմացիան</h1>
        <p>Ավելին իմանալու համար խնդրում ենք մուտք գործել անձնական հաշիվ կամ գրանցվել ռեգիստրացիայի բաժնում</p>
-       <Loading/>
   </div>
 }
--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import {Loading} from "../../components/loading"
+import { Loading } from "../../components/loading";
 
 export const Product = () => {
   const [product, setProduct] = useState(null);
