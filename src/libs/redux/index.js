0c57e2cf9c17c648b56d88e6df01fc3ced279cc4
SaqBaghdyan <baghdyan2000@gmail.com>
2024-11-08 12:33:18 +0400

    changes

--- src/libs/redux/index.js
+++ src/libs/redux/index.js
@@ -1,10 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { productReducer } from "./product-slice";
+import { configureStore } from '@reduxjs/toolkit'
+import { productReducer } from './product-slice'
 
 const store = configureStore({
   reducer: {
-    products: productReducer,
+   products: productReducer,
   },
-});
+})
 
-export default store;
+export default store
