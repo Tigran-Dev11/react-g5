bcff83f8732986607c756d2c34cce94cdf00c15f
SaqBaghdyan <baghdyan2000@gmail.com>
2024-11-26 16:31:10 +0400

    AddTodoList

--- src/pages/todo/index.jsx
+++ src/pages/todo/index.jsx
@@ -1,9 +1,63 @@
+import { useState } from "react"
 import { TodoComponent } from "../../components/todoComponent"
 import * as S from "./styled"
 
 export const TodoList=()=>{
-    return <div>
-        <TodoComponent/>
-    </div>
+    
 
+         const [todos, setTodos] = useState([
+ {
+ id: 1,
+ text: 'Doctor Appointment',
+ completed: true
+ },
+ {
+ id: 2,
+ text: 'Meeting at School',
+ completed: false
+ }
+ ]);
+ 
+ const [value, setValue] = useState('');
+function addTodo(value) {
+ const newTodo = {
+ id: Date.now(),
+ value,
+ completed: false
+ };
+ setTodos([...todos, newTodo]);
+ setValue('');
+ }
+function deleteTodo(id) {
+ setTodos(todos.filter(todo => todo.id !== id));
+ }
+function toggleCompleted(id) {
+ setTodos(todos.map(todo => {
+ if (todo.id === id) {
+ return {...todo, completed: !todo.completed};
+ } else {
+ return todo;
+ } 
+ }));
+ }
+return (
+ <div className="todo-list">
+ {todos.map(todo => (
+ <TodoComponent
+ key={todo.id} 
+ todo={todo}
+ deleteTodo={deleteTodo}
+ toggleCompleted={toggleCompleted} 
+ />
+ ))}
+<input
+ value={value}
+ onChange={e => setValue(e.target.value)} 
+ />
+<button onClick={() => addTodo(value)}>Add</button>
+ </div>
+ );
 }
+export default TodoList;
+
+ 
